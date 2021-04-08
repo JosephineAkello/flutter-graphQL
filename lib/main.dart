736c6e3e82e6a9e1019f06f1c93ae72ab261c336
JosephineAkello <josephineakello2030@gmail.com>
2021-04-08 14:36:09 +0300

    import login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,14 +13,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter GraphQL App',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primaryColor: Color(0xFF102253),
+        accentColor: Color(0xFF91A6E0),
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter GraphQL App',
         ),
-        centerTitle: true,
-    )));
+        centerTitle: true,),
+        body: Login(),));
   }
 }
 
