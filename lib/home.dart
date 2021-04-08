d64f4e89e83a7ceacaf2bf4e9c4dfc841fb1d937
JosephineAkello <josephineakello2030@gmail.com>
2021-04-08 13:45:39 +0300

    create homepage

---
+++ lib/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      body: Center(
+        child: Text('Yeey! Welcome!!'),)
+    );
+  }
+}
