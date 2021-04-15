d25d2df744f708b5fcc5a87846ddcd5ebc734c80
JosephineAkello <josephineakello2030@gmail.com>
2021-04-16 00:55:59 +0300

    add graphQL client details

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'register.dart';
+import 'graphqlConf.dart';
 
-void main() {
-  runApp(MyApp());
+GraphQLConfiguration graphQLConfiguration = GraphQLConfiguration();
+
+void main() async {
+  // runApp( MyApp());
+  await initHiveForFlutter();
+  runApp(GraphQLProvider(
+      client: graphQLConfiguration.client,
+      child: CacheProvider(child: MyApp())));
 }
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter GraphQL App',
-      theme: ThemeData(
-        primaryColor: Color(0xFF102253),
-        accentColor: Color(0xFF91A6E0),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter GraphQL App',
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter GraphQL App',
+        theme: ThemeData(
+          primaryColor: Color(0xFF102253),
+          accentColor: Color(0xFF91A6E0),
         ),
-        centerTitle: true,),
-        body: Login(),));
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Flutter GraphQL App',
+            ),
+            centerTitle: true,
+          ),
+          body: Register(),
+        ));
   }
 }
-
