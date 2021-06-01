ebfd15d64fe58cd890a8df1cb1352e1e30a72dbd
JosephineAkello <josephineakello2030@gmail.com>
2021-06-01 21:53:18 +0300

    update changes

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'home.dart';
+import 'querymutation.dart';
+
+QueryMutation queryMutation = QueryMutation();
 
 class Login extends StatefulWidget {
   createState() {
@@ -11,27 +15,30 @@ class LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   bool _obscureText = true;
 
+  final emailLoginTextController = TextEditingController();
+  final passwordLoginTextController = TextEditingController();
+
   Widget build(context) {
-      return Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text(
             'Flutter GraphQL App',
           ),
           centerTitle: true,
         ),
-    body: SingleChildScrollView(
-      child: Form(
-          key: _formKey,
-          child: Container(
-              padding: EdgeInsets.all(20.0),
-              child: Column(children: [
-                emailDetails(),
-                spaces(),
-                passwordDetails(),
-                spaces(),
-                loginButton(),  
-              ]))),
-      ));
+        body: SingleChildScrollView(
+          child: Form(
+              key: _formKey,
+              child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(children: [
+                    emailDetails(),
+                    spaces(),
+                    passwordDetails(),
+                    spaces(),
+                    loginButton(),
+                  ]))),
+        ));
   }
 
   Widget spaces() {
@@ -41,6 +48,7 @@ class LoginState extends State<Login> {
   Widget emailDetails() {
     return TextFormField(
         keyboardType: TextInputType.emailAddress,
+        controller: emailLoginTextController,
         decoration: InputDecoration(
           labelText: 'email',
           hintText: 'mojo@gmail.com',
@@ -63,6 +71,7 @@ class LoginState extends State<Login> {
   Widget passwordDetails() {
     return TextFormField(
         obscureText: _obscureText,
+        controller: passwordLoginTextController,
         decoration: InputDecoration(
           labelText: 'password',
           hintText: 'password',
@@ -83,20 +92,42 @@ class LoginState extends State<Login> {
   }
 
   Widget loginButton() {
-    return ElevatedButton(
-      onPressed: () {
-        //will enable non-nullable here
-        if (_formKey.currentState.validate()) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              backgroundColor: Color(0xFF9DACD4), content: Text('Processing')));
-        
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Home(),
-            ));
-      }},
-      child: Text('Login'),
-    );
+            //   return ElevatedButton(
+            // onPressed: () async {
+            //   //will enable non-nullable here
+            //   if (_formKey.currentState.validate()) {
+            //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            //         backgroundColor: Color(0xFF9DACD4),
+            //         content: Text('Processing')));
+       return Query(
+        options: QueryOptions(
+          document: gql(QueryMutation().loginUser),
+          variables: {
+            "email": emailLoginTextController.text,
+            "password": passwordLoginTextController.text
+          },
+        ),
+        builder: (QueryResult result,
+            {VoidCallback refetch, FetchMore fetchMore}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.isLoading) {
+            return Text('Login you in...');
+          }
+
+          final Map userInfo = result.data['findFirstUser'];
+          final String userId = userInfo['id'];
+          final String username = userInfo['name'];
+
+          print('$userId');
+          print('$username');
+            return ElevatedButton(
+              child: Text('Login'),
+              onPressed: (){
+              refetch();
+            });
+        });
   }
 }
